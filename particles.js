2c5d4959dae66dc7239fd7e5018d334abfed4b30
z35437sk <sambbhav.khare@student.manchester.ac.uk>
2022-08-12 08:30:42 +0530

    particle animation testing

---
+++ particles.js
@@ -0,0 +1,17 @@
+particlesJS('particles-js', {
+    canvas: {
+      
+      opacity: 1
+    },
+    particles: {
+      color_hex: '#fff',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 200,
+      anim: {
+        speed: 2
+      }
+    },
+    retina_detect: true
+  });
